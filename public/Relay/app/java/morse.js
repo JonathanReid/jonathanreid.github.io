5992537e8fe99fad64a47f86e623658999d18b7a
Jon Reid <jon.kablamo@gmail.com>
2016-01-06 23:55:54 +0000

    fixing finding ip

--- public/Relay/app/java/morse.js
+++ public/Relay/app/java/morse.js
@@ -14,7 +14,7 @@ var overlay;
 function init () {
   output = document.getElementById ("output");
   overlay = document.getElementById ("overlay");
-  doWebSocket ();
+  findServers();
 }
 
 function doWebSocket () {
@@ -70,5 +70,63 @@ function Next()
 {
   websocket.send ("2");
 }
+
+function findServers(port, ipBase, ipLow, ipHigh, maxInFlight, timeout, cb) {
+    var ipCurrent = +ipLow, numInFlight = 0, servers = [];
+    ipHigh = +ipHigh;
+
+    function tryOne(ip) {
+        ++numInFlight;
+        var address = "ws://" + ipBase + ip + ":" + port +"/Morse";
+        var socket = new WebSocket(address);
+        var timer = setTimeout(function() {
+            // console.log(address + " timeout");
+            var s = socket;
+            socket = null;
+            s.close();
+            --numInFlight;
+            next();
+        }, timeout);
+        socket.onopen = function() {
+            if (socket) {
+                console.log(address + " success");
+                clearTimeout(timer);
+                servers.push(socket.url);
+                --numInFlight;
+                cb(servers);
+                // next();
+            }
+        };
+        socket.onerror = function(err) {
+            if (socket) {
+                console.log(address + " error");
+                clearTimeout(timer);
+                --numInFlight;
+                next();
+            }
+        }
+    }
+
+    function next() {
+        while (ipCurrent <= ipHigh && numInFlight < maxInFlight) {
+            tryOne(ipCurrent++);
+        }
+        // if we get here and there are no requests in flight, then
+        // we must be done
+        if (numInFlight === 0) {
+            // console.log(servers);
+            cb(servers);
+        }
+    }
+
+    next();
+}
+
+findServers(4649, "192.168.0.", 1, 255, 20, 4000, function(servers) {
+    
+    url = servers[0];
+    console.log(url);
+    doWebSocket ();
+});
 
 window.addEventListener ("load", init, false);
