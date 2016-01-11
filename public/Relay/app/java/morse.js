df3a5ca38ccf893a4f67d4f9a867da0d8c37e3d3
Jon Reid <jon.kablamo@gmail.com>
2016-01-11 00:47:43 +0000

    Figured out why pairing wouldn't work

--- public/Relay/app/java/morse.js
+++ public/Relay/app/java/morse.js
@@ -350,12 +350,6 @@ function TryConnectToIP(ip)
       }
     }
 
-    sock.onclose = function (e)
-    {
-      foundServer = false;
-      connected = false;
-    }
-
     document.addEventListener('kill', function (e) {
       if(sock)
       {
@@ -365,12 +359,12 @@ function TryConnectToIP(ip)
     }, false);
 
     document.addEventListener('sendUID', function (e) {
-      sock.send(uidInput+":");
+      
       if(foundServer)
       {
         if(sock)
         {
-          
+          sock.send(uidInput+":");    
           console.log("Send UID event");
         }
       }
